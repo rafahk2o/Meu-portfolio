f45af0fa3331f5f70eab9058160f480181a8d984
rafahk2o <49276128+rafahk2o@users.noreply.github.com>
2022-08-23 12:10:06 -0300

    responsivo

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 let toggle = document.querySelector(".toggle");
 let circle = document.querySelector(".circle");
+let toggle2 = document.querySelector(".toggle2");
+let circle2 = document.querySelector(".circle2");
 let body = document.querySelector("body");
 let header = document.querySelector("header");
+let menuNav = document.querySelector(".menu-nav2");
+let btnMenu = document.querySelector(".bx ");
 
 circle.addEventListener("click", () => {
-body.classList.toggle('active');
-header.classList.toggle('active');
-circle.classList.toggle('active');
-toggle.classList.toggle('active');
+  body.classList.toggle("active");
+  header.classList.toggle("active");
+  circle.classList.toggle("active");
+  toggle.classList.toggle("active");
+});
+circle2.addEventListener("click", () => {
+  body.classList.toggle("active");
+  header.classList.toggle("active");
+  circle2.classList.toggle("active");
+  toggle2.classList.toggle("active");
+});
+
+btnMenu.addEventListener("click", () => {
+menuNav.classList.toggle("active");
 });
